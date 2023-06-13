3f62c48c3c242745ae609bf37f3db4f7ceb8ed78
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-06-13 21:58:11 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { access, writeFile } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+
+  access(filePath, (err) => {
+    if(err) {
+      writeFile(filePath, 'I am fresh and young', (error) => {
+        if(error) throw error;
+      });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  })
 };
 
 await create();
