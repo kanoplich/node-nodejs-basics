b65f008ac980bcdf74fd34de6b3e56c9b427f668
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-06-16 20:02:09 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path';
+import { readdir } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files');
+
+  try {
+    const files = await readdir(filePath);
+
+    const list = files.map((file) => file);
+    console.log(list);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
