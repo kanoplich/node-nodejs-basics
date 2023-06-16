6d75139ec120dd3ebeb405c8aeefe0584993af24
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-06-16 19:40:56 +0300

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { unlink } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await unlink(filePath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
