5b6a98e77724b45b408495a37fbbe76f64bd6e50
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-06-17 22:00:02 +0300

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const readStream = createReadStream(filePath);
+
+  readStream.on('readable', () => {
+    const data = readStream.read();
+    if(data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest('hex'));
+    }
+  });
 };
 
 await calculateHash();
