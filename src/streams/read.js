2701e1061d9bd14d16205566b65e15b387d28795
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-06-17 18:03:05 +0300

    feat: add streams functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url'; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  const readStream = createReadStream(filePath);
+  readStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    const chunkString = chunk.toString().trim();
+    const chunkReverse = chunkString.split('').reverse().join('');
+    this.push(chunkReverse + '\n');
+    callback();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  await pipeline(process.stdin, reverse, process.stdout);
 };
 
 await transform();
