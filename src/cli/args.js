69400255f16b3fad6d31221635638a46f1c77a2f
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-06-17 21:28:12 +0300

    feat: add cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+import { argv } from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const array = [];
+
+  for(let i = 0; i < argv.length; i++) {
+    if(argv[i].includes('--')) {
+      const elem = argv[i].slice(2);
+      array.push(`${elem} is ${argv[i + 1]}`);
+    }
+  }
+  console.log(array.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
-    // Write your code here 
+  const result = Object.entries(process.env).filter(([key, value]) => key.includes('RSS_')).map(([key, value]) => `${key}=${value}`).join('; ');
+  console.log(result);
 };
 
 parseEnv();
