313df1610c11401b0757207bc9b5228d2e7667f9
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-06-18 18:42:28 +0300

    feat: add cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathFile = join(__dirname, 'files', 'script.js');
+  fork(pathFile, args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess([1, 2, 3]);
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,16 +1,16 @@
 import { Transform } from 'stream';
 import { pipeline } from 'stream/promises';
 
-const reverse = new Transform({
-  transform(chunk, encoding, callback) {
-    const chunkString = chunk.toString().trim();
-    const chunkReverse = chunkString.split('').reverse().join('');
-    this.push(chunkReverse + '\n');
-    callback();
-  },
-});
-
 const transform = async () => {
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      const chunkString = chunk.toString().trim();
+      const chunkReverse = chunkString.split('').reverse().join('');
+      this.push(chunkReverse + '\n');
+      callback();
+    },
+  });
+
   await pipeline(process.stdin, reverse, process.stdout);
 };
 
